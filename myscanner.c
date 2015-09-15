4e6e6537cc2065031b1000d5ab016ba349e44a57
pedronovaes <pedromarcelino2006@hotmail.com>
2015-09-15 18:47:01 -0300

    Added token list and minor bug fixes

--- myscanner.c
+++ myscanner.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
+#include <stdbool.h>
 
 /*
 {NUMBER}+{ID}					1
@@ -34,15 +35,66 @@ extern int yylex();
 extern int yylineno;
 extern char *yytext;
 
-int main(){
+typedef struct token{
+	char *valor;
+	int classe;
+	int linha;
+	struct token *prox;
+}Token, *pToken;
 
+void inicializa_token(pToken *l);
+void insere_token(pToken *l, char *token, int classe_token, int linha_token);
+void imprime_lista(pToken l);
+
+
+int main(){
 	int ntoken, vtoken;
+	pToken p;
+
+	inicializa_token(&p);
 	ntoken = yylex();
 	while(ntoken){
-		printf("LINE: %d TOKEN: %s TYPE: %d\n", yylineno, yytext, ntoken);
+		//printf("LINE: %d TOKEN: %s TYPE: %d\n", yylineno, yytext, ntoken);
+		insere_token(&p,yytext,ntoken,yylineno);
 		ntoken = yylex();
 	}
+	imprime_lista(p);
 
 	return 0;
 }
 
+void inicializa_token(pToken *l){
+	*l = NULL;
+}
+
+void insere_token(pToken *l, char *token, int classe_token, int linha_token){
+	pToken p, q;
+	
+	p = (Token *)malloc(sizeof(Token));
+	p->valor = (char *)malloc(sizeof(char)*strlen(token));
+	if(classe_token == 3){
+		strcpy(p->valor,(token)+1);
+		p->valor[strlen(token)-2] = '\0';
+	}else
+		strcpy(p->valor,token);
+	p->classe = classe_token;
+	p->linha = linha_token;
+	if(*l == NULL){
+		*l = p;
+		return;
+	}
+	for(q = *l; (q && (q->prox != NULL)); q = q->prox);
+	q->prox = p;
+	p->prox = NULL;
+
+}
+
+void imprime_lista(pToken l){
+	pToken p;
+
+	for(p = l; (p); p = p->prox){
+		printf("TOKEN: %s\n", p->valor);
+		printf("CLASSE: %d\n", p->classe);
+		printf("LINHA: %d\n\n", p->linha);
+	}
+}
